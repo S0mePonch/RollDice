67d3891822e11a3909bd6f6d3c3c281b04307ec3
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-17 01:10:18 -0800

    Throw the dices of form random fixed

--- RollDice/ViewController.swift
+++ RollDice/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceOneView01: UIImageView!
+    @IBOutlet weak var diceTwoView02: UIImageView!
+    var rollLeftDice = 1;
+    var rollRightDice = 5;
+
+    @IBAction func btnRollDice(_ sender: UIButton) {
+        diceOneView01.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
+        
+        rollLeftDice += 1;
+        
+        diceTwoView02.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
+        
+        rollRightDice -= 1;
     }
-
-
+    
 }
 
