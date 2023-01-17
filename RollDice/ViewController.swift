c3a539d252dd0835f520c7704109d9db11c64316
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-16 16:54:45 -0800

    Initial Commit

---
+++ RollDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RollDice
+//
+//  Created by Alfonso on 26/10/1401 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
