f29722d205b2d07c1559cf79af0e5ee82ce96b13
Alfonso Cano Vargas <yveltal02341@outlook.com>
2023-01-17 01:32:22 -0800

    more simplified code

--- RollDice/ViewController.swift
+++ RollDice/ViewController.swift
@@ -11,17 +11,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceOneView01: UIImageView!
     @IBOutlet weak var diceTwoView02: UIImageView!
-    var rollLeftDice = 1;
-    var rollRightDice = 5;
+  
+    let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
 
     @IBAction func btnRollDice(_ sender: UIButton) {
-        diceOneView01.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
+        diceOneView01.image = diceArray.randomElement() //diceArray[Int.Random(in: 0...5)]
+                
+        diceTwoView02.image = diceArray.randomElement()
         
-        rollLeftDice += 1;
         
-        diceTwoView02.image = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)]
-        
-        rollRightDice -= 1;
     }
     
 }
